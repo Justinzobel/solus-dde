abfa98a4828093ec88420316a81f0bdc47de1efc
Justin Zobel <justin.zobel@gmail.com>
2020-03-27 12:28:55 +1030

    Small fixes

--- build-all.sh
+++ build-all.sh
@@ -12,11 +12,11 @@ rootdir="$(pwd)/"
 for i in $(cat ${rootdir}order)
   do
     pushd ${rootdir}${i}
-    solbuild build package.yml -p local-unstable-x86_64
+    ypkg -f package.yml
     if [[ $(ls -1 *.eopkg | wc -l) -gt 0 ]]
       then
         echo ${i} >> ${rootdir}success
-        mv *.eopkg /var/lib/solbuild/local/
+        eopkg it *.eopkg
         popd
       else
         echo ${i} >> ${rootdir}failures
